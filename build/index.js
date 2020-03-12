db8de14d93b256346d9e6dc42321b05fa75604ef
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-03-12 01:10:16 -0500

    Completed the requirements for part 2 but I would like to see if I can implement an enum with exports/modules in the browser

--- build/index.js
+++ build/index.js
@@ -2,6 +2,19 @@
 //     readonly name: string,
 //     sayHello(name): (name) => console.log(`Hello, ${name}`);
 // }
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // let friend01:IFriend = {name: "Melissa", sayHello: name};
 // let friend02:IFriend = {name: "Zach", sayHello: name};
 // let friend03:IFriend = {name: "Shay", sayHello: name};
@@ -24,12 +37,85 @@ var person03 = new Person("Melissa", "Birmingham", 31);
 var person04 = new Person("Melissa", "Birmingham", 32);
 var person05 = new Person("Melissa", "Birmingham", 32);
 var Vehicle = /** @class */ (function () {
-    function Vehicle(m, n) {
-        this.manufacturer = m;
-        this.wheelCount = n;
+    function Vehicle(manufacturer, numWheels) {
+        this.manufacturer = manufacturer;
+        this.wheelCount = numWheels;
     }
     Vehicle.prototype.aboutVehicle = function () {
-        console.log('');
+        console.log("This vehicle was made by " + this.manufacturer + " and has " + this.wheelCount + " wheels.");
     };
     return Vehicle;
 }());
+var Truck = /** @class */ (function (_super) {
+    __extends(Truck, _super);
+    function Truck(vehicleMake, vehicleWheels, vehicleHasDoors, vehicleHasBed) {
+        var _this = _super.call(this, vehicleMake, vehicleWheels) || this;
+        _this.hasDoors = true;
+        _this.hasBed = true;
+        _this.vehicleWheels = 4;
+        _this.hasDoors = vehicleHasDoors;
+        _this.hasBed = vehicleHasBed;
+        return _this;
+    }
+    Truck.prototype.aboutVehicle = function () {
+        console.log("This truck was made by " + this.manufacturer + " and has " + this.wheelCount + " wheels. It " + (this.hasBed ? 'has a bed' : 'has no bed') + " and " + (this.hasDoors ? 'has doors' : 'does not have doors') + ".");
+    };
+    return Truck;
+}(Vehicle));
+var Sedan = /** @class */ (function (_super) {
+    __extends(Sedan, _super);
+    function Sedan(vehicleMake, vehicleWheels, vehicleHasDoors, vehicleHasBed, vehicleSize, mpg) {
+        var _this = _super.call(this, vehicleMake, vehicleWheels) || this;
+        _this.hasDoors = true;
+        _this.hasBed = false;
+        _this.vehicleWheels = 4;
+        _this.hasDoors = vehicleHasDoors;
+        _this.hasBed = vehicleHasBed;
+        _this.carSize = vehicleSize;
+        _this.fuelEconomy = mpg;
+        return _this;
+    }
+    Sedan.prototype.aboutVehicle = function () {
+        console.log("This car was made by " + this.manufacturer + " and has " + this.wheelCount + " wheels. It " + (this.hasBed ? 'has a bed' : 'has no bed') + " and " + (this.hasDoors ? 'has doors' : 'does not have doors') + ". It is " + this.carSize + "-sized and gets " + this.fuelEconomy + " mpg.");
+    };
+    return Sedan;
+}(Vehicle));
+// export namespace CarSize {
+//     export enum Size {
+//         Subcompact,
+//         Compact,
+//         Small,
+//         Midsize,
+//         Medium,
+//         Large
+//     }
+// }
+var Motorcycle = /** @class */ (function (_super) {
+    __extends(Motorcycle, _super);
+    function Motorcycle(vehicleMake, vehicleWheels, vehicleHasDoors, vehicleHasBed, mpg, handlebars, steeringWheel) {
+        var _this = _super.call(this, vehicleMake, vehicleWheels) || this;
+        _this.hasDoors = false;
+        _this.hasBed = false;
+        _this.vehicleWheels = 2;
+        _this.hasSteeringWheel = false;
+        _this.hasHandleBars = true;
+        _this.hasDoors = vehicleHasDoors;
+        _this.hasBed = vehicleHasBed;
+        _this.fuelEconomy = mpg;
+        _this.hasHandleBars = handlebars;
+        _this.hasSteeringWheel = steeringWheel;
+        return _this;
+    }
+    Motorcycle.prototype.aboutVehicle = function () {
+        console.log("This motorcycle was made by " + this.manufacturer + " and has " + this.wheelCount + " wheels. It " + (this.hasBed ? 'has a bed' : 'has no bed') + " and " + (this.hasDoors ? 'has doors' : 'does not have doors') + ". It gets " + this.fuelEconomy + " mpg. It is controlled with " + (this.hasHandleBars ? 'handlebars.' : this.hasSteeringWheel ? 'a steering wheel somehow on a motorcyle??' : '10% luck, 20% skill, 15% concentrated power of will.'));
+    };
+    return Motorcycle;
+}(Vehicle));
+var parentVehicle = new Vehicle('GM', 4);
+parentVehicle.aboutVehicle();
+var childTruck = new Truck('Ford', 4, true, true);
+childTruck.aboutVehicle();
+var childSedan = new Sedan('Nissan', 4, true, false, 'Compact', 34);
+childSedan.aboutVehicle();
+var childMotorcycle = new Motorcycle('Honda', 2, false, false, 78, false, false);
+childMotorcycle.aboutVehicle();
--- src/index.ts
+++ src/index.ts
@@ -32,18 +32,99 @@ let person03 = new Person("Melissa", "Birmingham", 31);
 let person04 = new Person("Melissa", "Birmingham", 32);
 let person05 = new Person("Melissa", "Birmingham", 32);
 
+
+
 class Vehicle {
     manufacturer: string;
     wheelCount: number;
-    hasDoors: boolean;
-    hasReverse: boolean;
 
-    constructor(m: string, n: number) {
-        this.manufacturer = m;
-        this.wheelCount = n;
+    constructor(manufacturer: string, numWheels: number) {
+        this.manufacturer = manufacturer;
+        this.wheelCount = numWheels;
     }
 
     aboutVehicle() {
-        console.log('');
+        console.log(`This vehicle was made by ${this.manufacturer} and has ${this.wheelCount} wheels.`);
     }
 }
+
+class Truck extends Vehicle {
+    hasDoors: boolean = true;
+    hasBed: boolean = true;
+    vehicleWheels: number = 4;
+
+    constructor(vehicleMake: string, vehicleWheels: number, vehicleHasDoors: boolean, vehicleHasBed: boolean) {
+        super(vehicleMake, vehicleWheels);
+        this.hasDoors = vehicleHasDoors;
+        this.hasBed = vehicleHasBed;
+    }
+
+    aboutVehicle() {
+        console.log(`This truck was made by ${this.manufacturer} and has ${this.wheelCount} wheels. It ${this.hasBed ? 'has a bed' : 'has no bed'} and ${this.hasDoors ? 'has doors' : 'does not have doors'}.`);
+    }
+}
+
+class Sedan extends Vehicle {
+    hasDoors: boolean = true;
+    hasBed: boolean = false;
+    // carSize: Enumerator;
+    carSize: string;
+    fuelEconomy: number;
+    vehicleWheels: number = 4;
+    
+    constructor(vehicleMake: string, vehicleWheels: number, vehicleHasDoors: boolean, vehicleHasBed: boolean, vehicleSize: string, mpg: number) {
+        super(vehicleMake, vehicleWheels);
+        this.hasDoors = vehicleHasDoors;
+        this.hasBed = vehicleHasBed;
+        this.carSize = vehicleSize;
+        this.fuelEconomy = mpg;
+    }
+    aboutVehicle() {
+        console.log(`This car was made by ${this.manufacturer} and has ${this.wheelCount} wheels. It ${this.hasBed ? 'has a bed' : 'has no bed'} and ${this.hasDoors ? 'has doors' : 'does not have doors'}. It is ${this.carSize}-sized and gets ${this.fuelEconomy} mpg.`);
+    }
+}
+
+// export namespace CarSize {
+//     export enum Size {
+//         Subcompact,
+//         Compact,
+//         Small,
+//         Midsize,
+//         Medium,
+//         Large
+//     }
+// }
+
+class Motorcycle extends Vehicle {
+    hasDoors: boolean = false;
+    hasBed: boolean = false;
+    fuelEconomy: number;
+    vehicleWheels: number = 2;
+    hasSteeringWheel: boolean = false;
+    hasHandleBars: boolean = true;
+    
+    constructor(vehicleMake: string, vehicleWheels: number, vehicleHasDoors: boolean, vehicleHasBed: boolean, mpg: number, handlebars: boolean, steeringWheel: boolean) {
+        super(vehicleMake, vehicleWheels);
+        this.hasDoors = vehicleHasDoors;
+        this.hasBed = vehicleHasBed;
+        this.fuelEconomy = mpg;
+        this.hasHandleBars = handlebars;
+        this.hasSteeringWheel = steeringWheel;
+    }
+
+    aboutVehicle() {
+        console.log(`This motorcycle was made by ${this.manufacturer} and has ${this.wheelCount} wheels. It ${this.hasBed ? 'has a bed' : 'has no bed'} and ${this.hasDoors ? 'has doors' : 'does not have doors'}. It gets ${this.fuelEconomy} mpg. It is controlled with ${this.hasHandleBars ? 'handlebars.' : this.hasSteeringWheel ? 'a steering wheel somehow on a motorcyle??' : '10% luck, 20% skill, 15% concentrated power of will.'}`);
+    }
+}
+
+const parentVehicle = new Vehicle('GM', 4);
+parentVehicle.aboutVehicle();
+
+const childTruck = new Truck('Ford', 4, true, true);
+childTruck.aboutVehicle();
+
+const childSedan = new Sedan('Nissan', 4, true, false, 'Compact', 34);
+childSedan.aboutVehicle();
+
+const childMotorcycle = new Motorcycle('Honda', 2, false, false, 78, false, false);
+childMotorcycle.aboutVehicle();
